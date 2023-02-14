908420e04b398936e1c065107ee55257390614f5
gabgalge <itsgabgalge@gmail.com>
2023-02-15 03:55:22 +0800

    02/14

--- app/src/main/java/com/example/topjuantech_ojt/UserHotelReservation.java
+++ app/src/main/java/com/example/topjuantech_ojt/UserHotelReservation.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Reservation extends AppCompatActivity {
+public class UserHotelReservation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reservation);
+        setContentView(R.layout.activity_user_hotel_reservation);
     }
 }
