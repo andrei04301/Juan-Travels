4a6087076a106ad8836d3de67ba4e579799895a7
gabgalge <itsgabgalge@gmail.com>
2023-02-15 14:52:18 +0800

    02/15

--- app/src/main/java/com/example/topjuantech_ojt/UserHotelReservation.java
+++ app/src/main/java/com/example/topjuantech_ojt/UserHotelReservation.java
@@ -2,13 +2,45 @@ package com.example.topjuantech_ojt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class UserHotelReservation extends AppCompatActivity {
+    EditText editTextDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_hotel_reservation);
+
+        editTextDate = findViewById(R.id.editTextDate);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        UserHotelReservation.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        editTextDate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
     }
+
 }
