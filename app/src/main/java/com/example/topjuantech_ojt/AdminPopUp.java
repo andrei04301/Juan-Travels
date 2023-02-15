74e61568b6537fad62b2d439d2308c4fbccba4cb
xander04301 <110802453+xander04301@users.noreply.github.com>
2023-02-15 21:23:19 +0800

    15/02/2023

--- app/src/main/java/com/example/topjuantech_ojt/AdminPopUp.java
+++ app/src/main/java/com/example/topjuantech_ojt/AdminPopUp.java
@@ -2,12 +2,29 @@ package com.example.topjuantech_ojt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AdminPopUp extends AppCompatActivity {
+    private EditText prodName, prodPrice;
+    private ImageButton prodImage;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    ProgressDialog progressDialog;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +32,34 @@ public class AdminPopUp extends AppCompatActivity {
         setContentView(R.layout.activity_admin_pop_up);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-
         getWindow().setLayout((int) (width*.7),(int)(height*.5));
-
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity= Gravity.CENTER;
-        params.x=-20;
-        params.y=0;
-
+        params.x=0;
+        params.y=-20;
         getWindow().setAttributes(params);
 
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        fStore = FirebaseFirestore.getInstance();
+
+        prodName = (EditText) findViewById(R.id.prodName);
+        prodPrice = (EditText) findViewById(R.id.prodPrice);
+        prodImage = findViewById(R.id.prodImage);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerformAuth();
+            }
+        });
+
+
+    }
+
+    private void PerformAuth() {
     }
 }
-//    java.lang.RuntimeException: Unable to start activity ComponentInfo{com.example.topjuantech_ojt/com.example.topjuantech_ojt.AdminPopUp}: java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity.
