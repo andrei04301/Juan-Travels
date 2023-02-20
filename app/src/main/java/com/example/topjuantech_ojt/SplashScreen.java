7253f57fab39f1521a370423e504c33242ed73a7
gabgalge <itsgabgalge@gmail.com>
2023-02-21 00:00:21 +0800

    02/20

---
+++ app/src/main/java/com/example/topjuantech_ojt/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.topjuantech_ojt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        }, 3800);
+    }
+}
