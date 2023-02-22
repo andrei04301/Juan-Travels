7654f2924f31a12376b13e2cc0c96ee69c7c78c4
gabgalge <itsgabgalge@gmail.com>
2023-02-23 02:13:32 +0800

    02/22

--- app/src/main/java/com/example/topjuantech_ojt/AdminBottomNav.java
+++ app/src/main/java/com/example/topjuantech_ojt/AdminBottomNav.java
@@ -1,49 +1,132 @@
-package com.example.topjuantech_ojt;
-
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-
-public class AdminBottomNav extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_drawer);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.user_btm_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.admin_home:
-                                setContentView(R.layout.activity_user_dashboard);
-                                return true;
-                            case R.id.admin_products:
-                                setContentView(R.layout.activity_user_foodspot);
-                                return true;
-                            case R.id.admin_booking:
-                                setContentView(R.layout.activity_user_gas);
-                                return true;
-                            case R.id.user_profile:
-                                setContentView(R.layout.activity_user_profile);
-                                return true;
-                        }
-                        return false;
-                    }
-                });
-    }
-}
+//package com.example.topjuantech_ojt;
+//
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.ActionBarDrawerToggle;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+//import androidx.drawerlayout.widget.DrawerLayout;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.MenuItem;
+//
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
+//import com.google.android.material.navigation.NavigationView;
+//
+//public class AdminBottomNav extends AppCompatActivity {
+//
+//
+//
+//    public class AdminBottomNav extends AppCompatActivity {
+//
+//        BottomNavigationView bottomNavigationView;
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_admin_bottom_nav);
+//
+//            bottomNavigationView = findViewById(R.id.admin_btm_navigation);
+//
+//            // set the listener for the bottom navigation view
+//            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    // determine which menu item was clicked
+//                    switch (item.getItemId()) {
+//                        case R.id.admin_home:
+//                            startActivity(new Intent(AdminBottomNav.this, AdminDashboard.class));
+//                            return true;
+//                        case R.id.admin_products:
+//                            startActivity(new Intent(AdminBottomNav.this, AdminOrders.class));
+//                            return true;
+//                        case R.id.admin_booking:
+//                            startActivity(new Intent(AdminBottomNav.this, AdminInventory.class));
+//                            return true;
+//                        case R.id.admin_profile:
+//                            startActivity(new Intent(AdminBottomNav.this, AdminProfile.class));
+//                            return true;
+//                    }
+//                    return false;
+//                }
+//            });
+//        }
+//    }
+//
+//}
+////    BottomNavigationView bottomNavigationView;
+////
+////    private void startActivityForMenuItem(MenuItem item) {
+////        Intent intent;
+////        switch (item.getItemId()) {
+////            case R.id.user_home:
+////                intent = new Intent(this, AdminDashboard.class);
+////                break;
+////            case R.id.user_orders:
+////                intent = new Intent(this, UserHotelReservation.class);
+////                break;
+////            case R.id.user_booking:
+////                intent = new Intent(this, UserGas.class);
+////                break;
+////            case R.id.user_profile:
+////                intent = new Intent(this, UserProfile.class);
+////                break;
+////            default:
+////                intent = new Intent(this, UserAttraction.class);
+////                break;
+////        }
+////        startActivity(intent);
+////    }
+////
+////    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_user_dashboard);
+////
+////        bottomNavigationView = findViewById(R.id.user_btm_navigation);
+////        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+////            @Override
+////            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+////                startActivityForMenuItem(item);
+////                return true;
+////            }
+////        });
+////    }
+////}
+//
+//
+//
+//
+//
+//
+////
+////    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_navigation_drawer);
+//
+////        BottomNavigationView bottomNavigationView = findViewById(R.id.user_btm_navigation);
+////        bottomNavigationView.setOnNavigationItemSelectedListener(
+////                new BottomNavigationView.OnNavigationItemSelectedListener() {
+////                    @Override
+////                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+////                        switch (item.getItemId()) {
+////                            case R.id.admin_home:
+////                                setContentView(R.layout.activity_user_dashboard);
+////                                return true;
+////                            case R.id.admin_products:
+////                                setContentView(R.layout.activity_user_foodspot);
+////                                return true;
+////                            case R.id.admin_booking:
+////                                setContentView(R.layout.activity_user_gas);
+////                                return true;
+////                            case R.id.user_profile:
+////                                setContentView(R.layout.activity_user_profile);
+////                                return true;
+//
+////                        return false;
+////                    }
+////                });
+////    }
+////}
--- app/src/main/java/com/example/topjuantech_ojt/UserBottomNav.java
+++ app/src/main/java/com/example/topjuantech_ojt/UserBottomNav.java
@@ -1,49 +1,128 @@
 package com.example.topjuantech_ojt;
 
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 public class UserBottomNav extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
+
+}
+//    BottomNavigationView bottomNavigationView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_user_bottom_nav);
+//
+//        bottomNavigationView = findViewById(R.id.user_btm_navigation);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                int itemId = item.getItemId();
+//                switch (itemId) {
+//                    case R.id.user_home:
+//                        startActivity(new Intent(UserBottomNav.this, UserDashboard.class));
+//                        return true;
+//                    case R.id.user_orders:
+//                        startActivity(new Intent(UserBottomNav.this, UserHotelReservation.class));
+//                        return true;
+//                    case R.id.user_booking:
+//                        startActivity(new Intent(UserBottomNav.this, UserGas.class));
+//                        return true;
+//                    case R.id.user_profile:
+//                        startActivity(new Intent(UserBottomNav.this, UserProfile.class));
+//                        return true;
+//                    default:
+//                        startActivity(new Intent(UserBottomNav.this, UserAttraction.class));
+//                        return true;
+//                }
+//            }
+//        });
+//    }
+//}
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_drawer);
+//    public class AdminBottomNav extends AppCompatActivity {
+//        BottomNavigationView bottomNavigationView;
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_admin_bottom_nav);
+//
+//            bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+//            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    int itemId = item.getItemId();
+//                    switch (itemId) {
+//                        case R.id.user_home:
+//                            startActivity(new Intent(AdminBottomNav.this, UserDashboard.class));
+//                            return true;
+//                        case R.id.user_orders:
+//                            startActivity(new Intent(AdminBottomNav.this, UserHotelReservation.class));
+//                            return true;
+//                        case R.id.user_booking:
+//                            startActivity(new Intent(AdminBottomNav.this, UserGas.class));
+//                            return true;
+//                        case R.id.user_profile:
+//                            startActivity(new Intent(AdminBottomNav.this, UserProfile.class));
+//                            return true;
+//                        default:
+//                            startActivity(new Intent(AdminBottomNav.this, UserAttraction.class));
+//                            return true;
+//                    }
+//                }
+//            });
+//        }
+//    }
+//
+//}
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.user_btm_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.user_home:
-                                setContentView(R.layout.activity_user_dashboard);
-                                return true;
-                            case R.id.user_orders:
-                                setContentView(R.layout.activity_user_foodspot);
-                                return true;
-                            case R.id.user_booking:
-                                setContentView(R.layout.activity_user_gas);
-                                return true;
-                            case R.id.user_profile:
-                                setContentView(R.layout.activity_user_profile);
-                                return true;
-                        }
-                        return false;
-                    }
-                });
-        }
-    }
+
+
+
+
+
+//    BottomNavigationView bottomNavigationView;
+//
+//    private void startActivityForMenuItem(MenuItem item) {
+//        Intent intent;
+//        switch (item.getItemId()) {
+//            case R.id.user_home:
+//                intent = new Intent(this, UserDashboard.class);
+//                break;
+//            case R.id.user_orders:
+//                intent = new Intent(this, UserHotelReservation.class);
+//                break;
+//            case R.id.user_booking:
+//                intent = new Intent(this, UserGas.class);
+//                break;
+//            case R.id.user_profile:
+//                intent = new Intent(this, UserProfile.class);
+//                break;
+//            default:
+//                intent = new Intent(this, UserAttraction.class);
+//                break;
+//        }
+//        startActivity(intent);
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_user_dashboard);
+//
+//        bottomNavigationView = findViewById(R.id.user_btm_navigation);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                startActivityForMenuItem(item);
+//                return true;
+//            }
+//        });
+//    }
+//}
