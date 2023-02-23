d6f9b7e11ba43a6b7ed30843165bb6c5cdea7f7a
gabgalge <itsgabgalge@gmail.com>
2023-02-23 19:28:02 +0800

    02/23

--- app/src/main/java/com/example/topjuantech_ojt/MainActivity.java
+++ app/src/main/java/com/example/topjuantech_ojt/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.topjuantech_ojt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     //vars declaration
@@ -26,7 +29,13 @@ public class MainActivity extends AppCompatActivity {
         btn_signupManager = (Button) findViewById(R.id.btn_signupManager);
 
         //Animations
+        RelativeLayout relativeLayout = findViewById(R.id.mainAct);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         //button onTouch or redirection
         btn_signupUser.setOnTouchListener(new View.OnTouchListener() {
