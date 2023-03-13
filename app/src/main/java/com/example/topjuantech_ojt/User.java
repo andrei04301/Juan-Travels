91e6c2f82bdb8ad29949d8e7d30b5be618047c5f
xander04301 <110802453+xander04301@users.noreply.github.com>
2023-03-13 17:06:49 +0800

    13/03/2023

--- app/src/main/java/com/example/topjuantech_ojt/User.java
+++ app/src/main/java/com/example/topjuantech_ojt/User.java
@@ -3,15 +3,65 @@ package com.example.topjuantech_ojt;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    String EstablishmentName , City, AdminID, EstablishmentType;
+    String EstablishmentName , City, AdminID,
+            EstablishmentType, Firstname, Lastname,
+            PhoneNumber, Email, Status;
 
     public User(){}
 
-    public User(String establishmentName, String city, String adminID, String establishmentType) {
+    public User(String establishmentName, String city, String adminID,
+                String establishmentType, String firstname, String lastname,
+                String phoneNumber, String status) {
         EstablishmentName = establishmentName;
         City = city;
         AdminID = adminID;
         EstablishmentType = establishmentType;
+        Firstname = firstname;
+        Lastname= lastname;
+        PhoneNumber= phoneNumber;
+        Status=status;
+
+
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
     }
 
     public String getEstablishmentName() {
--- app/src/main/java/com/example/topjuantech_ojt/UserProfile.java
+++ app/src/main/java/com/example/topjuantech_ojt/UserProfile.java
@@ -2,10 +2,23 @@ package com.example.topjuantech_ojt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UserProfile extends AppCompatActivity {
-
+    private EditText firstname, lastname, phoneNumber,
+            email, status;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    ProgressDialog progressDialog;
+    FirebaseFirestore fStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
